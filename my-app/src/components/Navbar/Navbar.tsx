f440e8a64a208433040f7a7e86c2a4ee0eb9c0a1
dsanborn <djsanborn90@gmail.com>
2026-01-28 03:53:25 -0500

    Add projects dropdown

--- my-app/src/components/Navbar/Navbar.tsx
+++ my-app/src/components/Navbar/Navbar.tsx
@@ -1,24 +1,51 @@
 // Navbar.jsx
+import { useState } from 'react'
 import './Navbar.css'
+import './NavButton.css'
 import { APP_CONFIG } from '@/constants/appConfig'
 
-
 export default function Navbar() {
+  const [open, setOpen] = useState(false)
+
   return (
     <nav className="navbar">
       <ul>
         <li className="brand">
           <a href="/">{APP_CONFIG.name}</a>
         </li>
+
         <li>
           <a href="/">Home</a>
         </li>
+
         <li>
           <a href="/about">About</a>
         </li>
-        <li>
-          <a href="/projects">Projects</a>
+
+        <li className="nav-item dropdown">
+          {/* toggle button */}
+          <button
+            className="dropdown-toggle"
+            onClick={() => setOpen((prev) => !prev)}
+            aria-expanded={open}
+          >
+            Projects
+          </button>
+
+          {/* dropdown */}
+          <ul className={`dropdown-menu ${open ? 'open' : ''}`}>
+            <li>
+              <a href="/web">Web Dev</a>
+            </li>
+            <li>
+              <a href="/mobile">Mobile Apps</a>
+            </li>
+            <li>
+              <a href="/uiux">UI / UX</a>
+            </li>
+          </ul>
         </li>
+
         <li>
           <a href="/contact">Contact</a>
         </li>
