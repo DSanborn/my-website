f8ae77991d9e62802d1c7b9973a3dec6e59e44d9
dsanborn <djsanborn90@gmail.com>
2026-03-01 21:05:43 -0500

    Add firebase

--- my-app/src/components/UI/Logo.tsx
+++ my-app/src/components/UI/Logo.tsx
@@ -2,5 +2,9 @@ import LogoSVG from '@/assets/icon.svg'
 import './Logo.css'
 
 export default function Logo() {
-  return <img src={LogoSVG} className="logo neon-logo" alt="Logo" />
+  return (
+    <span role="img" aria-label="Logo">
+      <LogoSVG className="logo neon-logo" />
+    </span>
+  )
 }
